305771ff9721b230ae8a4c18c19e4f41f7e58558
Melaniaift <76125731+Melaniaift@users.noreply.github.com>
2024-10-09 08:52:57 +0300

    basic version

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import { Footer } from './components/Footer';
+import { Header } from './components/Header';
 import { AllRoutes } from './routes/AllRoutes';
 
 function App() {
   return (
     <div className="App">
+      <Header></Header>
       <AllRoutes></AllRoutes>
+      <Footer></Footer>
     </div>
   );
 }
